6e4788419ff8c74df321383e61555dd8a115ef4f
AlyoshaSlysh <92150672+AlyoshaSlysh@users.noreply.github.com>
2021-10-24 21:14:09 +0300

    git add .

--- src/js/index.js
+++ src/js/index.js
@@ -5,17 +5,12 @@ Swiper.use([Navigation, Pagination]);
 $(document).ready(function () {
     console.log("ready!");
 
-    // init Swiper:
-
     const swiper = new Swiper('.mySwiper', {
         loop: true,
 
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
     });
 });
-
-// import Swiper and modules styles
